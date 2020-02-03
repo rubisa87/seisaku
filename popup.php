a3447ee70d4459ac048fdfc6ac6e264a0346dd0b
rubisa87 <57693127+rubisa87@users.noreply.github.com>
2020-02-03 11:09:02 +0900

    Add files via upload

---
+++ popup.php
@@ -0,0 +1,14 @@
+  <table width="100%" background="" height="40px" />
+<tr>
+<td valign="top" >
+<div style="width:1230px;height:28px;font-size:16px;font-family:Arial;float:right;padding-top:9px;padding-right:25px;">
+<marquee onmouseover=this.stop() onmouseout=this.start() scrolldelay="50" scrollamount="5">
+<strong>
+<a href="/Đường dẫn tới thông báo của bạn" style="color:#fb4ca7">  QRコード決済導入について</a> 
+
+<a href="/Đường dẫn tới thông báo của bạn" style="color:#ff0">    確定申告はもうすぐ締め切りで、早めにしてください  </a>  
+</strong>
+</marquee>
+</div>
+</td></tr>
+</table>
