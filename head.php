8f11c3ab9c9faf9752706f80eccd1a84e4fc3109
rubisa87 <57693127+rubisa87@users.noreply.github.com>
2020-02-05 12:24:25 +0900

    Add files via upload

---
+++ head.php
@@ -0,0 +1,31 @@
+ <!-- function goi($name){ -->
+<!-- // echo " -->
+<body>
+
+<div class='header'>
+  <h1>勤怠管理システム</h1>
+  <p>Tran Duc Anh</p>
+ 
+</div>
+
+<div id='menu'>
+  <ul>
+    <li><a href='main.php'>ホーム</a></li>
+    <li><a href='self.php'>各自</a>
+      <ul class='sub-menu'>
+        <li><a href='#'>勤怠データ</a></li>
+        <li><a href='#'>給料</a></li>
+        <li><a href='kojin.php'>個人情報確認</a></li>
+      </ul>
+      </li>
+    <li><a href='kanri.php'>管理人</a>
+      <ul class='sub-menu'>
+        <li><a href='kanri.php'>勤怠データ一覧</a></li>
+        <li><a href='keisan.php'>給料清算</a></li>
+        <li><a href='kojin_ichiran'>個人情報一覧</a></li>
+      </ul>
+    </li>
+  </ul>
+</div>
+
+<div class='row'>
---
+++ shinshouhin.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+<title>打刻</title>
+<!-- <link rel="stylesheet" href="menu.css" type="text/css" /> -->
+<link rel="stylesheet" href="layout.css" type="text/css" />
+</head>
+<?php
+require "dbasename.php";
+require "head.php";
+require "popup.php";
+?>
+  <div class="leftcolumn">
+    <div class="card">
+
+    <h3>この先新発売商品まだありません</h3>
+
+
+</div>
+ </div>
+  <div class="rightcolumn">
+    <div class="card">
+      <h2>新着情報</h2>
+      <!-- <div class="fakeimg" style="height:100px;">Image</div> -->
+      <li><a href="shinshouhin.php">新商品が発売</a></li>
+      <li><a href="main.php">戻る</a></li>
+
+    </div>
+    <div class="card">
+      <h3>Popular Post</h3>
+      <div class="fakeimg"><p>Image</p></div>
+      <div class="fakeimg"><p>Image</p></div>
+      <div class="fakeimg"><p>Image</p></div>
+    </div>
+    <div class="card">
+      <h3>Follow Me</h3>
+      <p>Some text..</p>
+    </div>
+  </div>
+</div>
+
+<div class="footer">
+    <li><a href="#">Contact</a></li>
+
+  Copyright © 2019 Tran Duc Anh - 横浜システム工学院専門学校
+</div>
+
+</body>
+</html>
