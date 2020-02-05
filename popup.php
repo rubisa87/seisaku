1a6b9291a13eafada76249836ef598aff97faed8
rubisa87 <anhpro8793@gmail.com>
2020-02-05 13:45:50 +0900

    f

--- popup.php
+++ popup.php
@@ -1,7 +1,7 @@
   <table width="100%" background="" height="40px" />
 <tr>
 <td valign="top" >
-<div style="width:1230px;height:28px;font-size:16px;font-family:Arial;float:right;padding-top:9px;padding-right:25px;">
+<div style="width:80%;height:28px;font-size:16px;font-family:Arial;float:left;padding-top:9px;padding-right:25px;">
 <marquee onmouseover=this.stop() onmouseout=this.start() scrolldelay="50" scrollamount="5">
 
 <strong>
