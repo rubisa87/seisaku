b09cc6c89af1cd7e5c89fb3a3564863f40810ac4
rubisa87 <57693127+rubisa87@users.noreply.github.com>
2020-02-08 15:06:24 +0900

    Add files via upload

---
+++ setsumei.php
@@ -0,0 +1,14 @@
+<div ><h1 style="color:orangered">使い方について説明</h1>
+<P>本システムをご閲覧いただき、ありがとうございます
+<br>
+<br>
+「管理人」にアクセス方法：「各自」で店長のコードとパスワードを使ってログインしたら、管理人も入れます。<br>或いはpasscode: mise　、と　password は　8793 でログインします。<br>
+試す用のコードとパスワードは以下のテーブル通りとなっております。<br>
+気になる方は自由にお使いください。<br>
+   <img src="List.png">
+<!-- <img src="List.png" alt="Smiley face" height="42" width="42"> -->
+
+
+
+</p>
+</div>
