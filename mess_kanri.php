474cad7f6a06c9ae761b6860fece68951cfff7f8
rubisa87 <57693127+rubisa87@users.noreply.github.com>
2020-02-09 14:29:55 +0900

    Add files via upload

---
+++ mess_kanri.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+<title>打刻</title>
+<link rel="stylesheet" href="layout.css" type="text/css" />
+<script>
+ 
+// ---------------------cách 2 la đây ↓↓↓↓↓↓↓↓↓↓↓ーーーーーーーーーーーーーー
+$(document).ready(function(){
+
+      $("#theme1").click(function(){
+        $("#cont1").toggle();
+       
+    });
+      $("#theme2").click(function(){
+        $("#cont2").toggle();
+       
+    });
+      $("#theme3").click(function(){
+        $("#cont3").toggle();
+       
+    });
+});
+// ーーーーーーーーーーーーーーーーーーーーーーーーcách 2 chỉ từng nàyy↑↑↑↑↑↑↑↑↑↑---------------------------
+</script>
+
+
+</head>
+
+<?php
+require "dbasename.php";
+require "head.php";
+require "popup.php";
+
+ if ($_SESSION['login']==False){
+    //③SESSIONの「error2」に「ログインしてください」と設定する。
+    //④ログイン画面へ遷移する。
+// }
+    $_SESSION['error2'] ="ログインしてください";
+    header("Location: self_login.php?page=self");//④ログイン画面へ遷移する。
+} 
+  ?>
+ <div class="leftcolumn">
+    <div class="card">
+
+  <h2 style="color:#ffae6a" id="theme3">
+    メッセージ　
+    </h2>
+<table id="cont3" border="1" class="mess" >
+<tr><th >氏名</th><th >時間</th><th >内容</th><th >状態</th></tr>
+<?php
+$passcode=3;
+   $stmt = $pdo->query("SELECT * FROM messenger WHERE tousercode=$passcode");
+            // $stmt->execute(array($passcode));
+ // $st = $pdo->query("SELECT * FROM kintaidata where passcode= $passcode");
+  while ($row = $stmt->fetch()) {
+    $id = htmlspecialchars($row['id']);
+    $fromu = htmlspecialchars($row['fromusercode']);
+    $content = htmlspecialchars($row['content']);
+    $time = htmlspecialchars($row['time']);
+    $status = htmlspecialchars($row['status']);
+$ndb = $pdo->query("SELECT * FROM staffdata WHERE passcode= $fromu");
+    $a =$ndb->fetch();
+
+
+     echo "<tr><td>".$a['name']."</td><td>$time</td><td>$content</td><td>$status</td></tr>";
+
+  }
+  
+?>
+<tr><form method="post">
+<td><select name="name" style ="font-size: 9px"
+>
+            <?php
+   $stmt = $pdo->query("SELECT * FROM staffdata");
+while ($row = $stmt->fetch()) {
+  $code=$row['passcode'];
+  $name=$row['name'];
+                echo "<option>$name</option>";
+                // echo "<input type='hidden' name='code' value=".$code.">";
+              }
+            ?>
+          </select></td>
+<td><input type="hidden" name="from" value="<?php echo $_SESSION['code'];  ?>">
+  <!-- <input type="hidden" name="code" value="<?php echo $code;  ?>"> --></td>
+<td><textarea rows="3" cols="40" name="content">
+</textarea></td>
+<td><button type = 'submit' formmethod='POST' name ='soushin' >送信</button></td></form>
+</tr>
+</table>
+</div>
+  </div>
+<?php require "rightcolumn_kanri.php";?>
+</div>
+<div class="footer">
+    <li><a href="#">Contact</a></li>
+
+  Copyright © 2019 Tran Duc Anh - 横浜システム工学院専門学校
+</div>
+
+</body>
+</html>
